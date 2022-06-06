37f3435c418de60d8fdb7db40418f54cc48a2ec8
rajatpanwar001 <rajat.p@cryenx.com>
2022-06-06 10:57:39 +0530

    Forming Relationships

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import { createConnection } from 'typeorm';
+import express, { application } from 'express';
 import { Banker } from './entities/Banker';
 import { Client } from './entities/Client';
 import { Transaction } from './entities/Transaction';
+import { createClientRouter } from './routes/create_client';
+import { createBankerRouter } from './routes/create_banker';
+import { createTransactionRouter } from './routes/create_transaction';
+import { connectBankerToClientRouter } from './routes/connect_banker_to_client';
+
+const app = express();
 
 const main = async () => {
   try {
@@ -16,6 +23,16 @@ const main = async () => {
       synchronize: true,
     });
     console.log('Connected to Postgres');
+
+    app.use(express.json());
+    app.use(createClientRouter);
+    app.use(createBankerRouter);
+    app.use(createTransactionRouter);
+    app.use(connectBankerToClientRouter);
+
+    app.listen(8080, () => {
+      console.log('Listening on port 8080');
+    });
   } catch (error) {
     console.error(error);
     throw new Error('Unable to connect to db');
